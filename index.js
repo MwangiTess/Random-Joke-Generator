c8382227e6f68c3b53a57b804fe072af924aabf4
Teresia <teresiamwangi26@gmail.com>
2023-04-07 19:40:40 +0300

    Modifying index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn")
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist"
+
